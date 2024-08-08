0ac9744554c0bf7b8b295449c63ce61f7edfb219
Krish Verma <krishverma837@gmail.com>
2024-08-08 22:32:05 +0530

    Complete upto caption creation

--- src/components/ResultVideo.js
+++ src/components/ResultVideo.js
@@ -3,11 +3,14 @@ import SparklesIcon from "./SparklesIcon";
 import { FFmpeg } from "@ffmpeg/ffmpeg";
 import { fetchFile, toBlobURL } from "@ffmpeg/util";
 import { transcriptionItemsToSrt } from "@/libs/awsTranscriptionHelpers";
+import roboto from "@/fonts/Roboto-Regular.ttf";
+import robotoBold from "@/fonts/Roboto-Bold.ttf";
 
 export default function ResultVideo({filename, transcriptionItems}){
     const videoUrl = ""+filename;
-    // const [videoSrc, setVideoSrc] = useState('');
     const [loaded, setLoaded] = useState(false);
+    const [primaryColour, setPrimaryColour] = useState('#FFFFFF')
+    const [outlineColour, setOutlineColour] = useState('#000000')
     const ffmpegRef = useRef(new FFmpeg());
     const videoRef = useRef(null);
 
@@ -24,17 +27,33 @@ export default function ResultVideo({filename, transcriptionItems}){
             coreURL: await toBlobURL(`${baseURL}/ffmpeg-core.js`, 'text/javascript'),
             wasmURL: await toBlobURL(`${baseURL}/ffmpeg-core.wasm`, 'application/wasm'),
         });
+        await ffmpeg.writeFile('@/fonts/Roboto-Regular.ttf', await fetchFile(roboto))
+        await ffmpeg.writeFile('@/fonts/Roboto-Bold.ttf', await fetchFile(robotoBold))
         setLoaded(true);
+    }
+
+    function toFFmpegColor(rgb){
+        const bgr = rgb.slice(5,7) + rgb.slice(3,5) + rgb.slice(1,3);
+        console.log({rgb,bgr});
+        return '$H' + bgr + '$';
     }
 
     const transcode = async () => {
         const ffmpeg = ffmpegRef.current;
-        await ffmpeg.writeFile(filename, await fetchFile(videoUrl));
         const srt = transcriptionItemsToSrt(transcriptionItems);
+        await ffmpeg.writeFile(filename, await fetchFile(videoUrl));
+        await ffmpeg.writeFile('subs.srt', srt);
+        
         ffmpeg.on('log', ({ message }) => {
             console.log(message);
         });
-        await ffmpeg.exec(['-i', filename, 'output.mp4']);
+        await ffmpeg.exec([
+            '-i', filename, 
+            // '-to', '00:00:05',
+            '-preset', 'ultrafast',
+            '-vf', `subtitles=subs.srt:fontsdir=/tmp:force_style='Fontname=Roboto, FontSize=30, MarginV=100, PrimaryColour=H${toFFmpegColor(primaryColour)}, OutlineColour=${toFFmpegColor(outlineColour)}'`,
+            'output.mp4'
+        ]);
         const data = await ffmpeg.readFile('output.mp4');
         videoRef.current.src =
             URL.createObjectURL(new Blob([data.buffer], {type: 'video/mp4'}));
@@ -49,6 +68,19 @@ export default function ResultVideo({filename, transcriptionItems}){
             <SparklesIcon />
             <span>Apply Captions</span>
             </button>
+        </div>
+        <div>
+            primary colour:
+            <input type="color" 
+                   className="border-0 p-0 m-0"
+                   value={primaryColour}
+                   onChange={ev => setPrimaryColour(ev.target.value)} />
+            <br />
+            outline colour:
+            <input type="color"
+                   className="border-0"
+                   value={outlineColour}
+                   onChange={ev => setOutlineColour(ev.target.value)} />
         </div>
         <div className="rounded-xl overflow-hidden">
             <video 
